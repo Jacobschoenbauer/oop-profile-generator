026bbb8c2a7b2269034482fea730059ee7a440ff
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-05 15:33:37 -0500

    set up engineer intern and manger external files

--- _test_/employee.test.js
+++ _test_/employee.test.js
@@ -1,11 +1,10 @@
 const Employee = require("../lib/employee");
-describe("Employee", () => {
-  it("create a employee with name id email", () => {
-    const employee = new Employee("Sarah", "3", "jhwt65@yahoo.com");
 
-    // Verify that the new object has the correct properties
-    expect(employee.name).toEqual("Sarah");
-    expect(employee.age).toEqual("3");
-    expect(employee.email).toEqual("jhwt65@yahoo.com");
-  });
+const employee = new Employee("Sarah", "3", "jhwt65@yahoo.com");
+test("create a employee with name id email", () => {
+  // Verify that the new object has the correct properties
+
+  expect(employee.name).toEqual("Sarah");
+  expect(employee.age).toEqual("3");
+  expect(employee.email).toEqual("jhwt65@yahoo.com");
 });
